0e88cf17ec0ecc9f9ac3039f8d85bf6fd3176a53
rakisuto <tama5.bo6bo6@gmail.com>
2025-09-05 13:43:32 +0900

    fix: add missing description for standard regulation in Page component

--- ygo-friend-app/app/page.tsx
+++ ygo-friend-app/app/page.tsx
@@ -5,6 +5,7 @@ import limited from "../data/limited.json";
 import semiLimited from "../data/semi_limited.json";
 import './globals.css';
 
+// スタンダードで構築することという記載があったほうが良いか？
 export default function Page() {
   return (
     <main style={{ padding: "1.5rem" }}>
@@ -14,7 +15,9 @@ export default function Page() {
           （最終更新: 2025年9月4日）
         </span>
       </h1>
-
+      <h2 style={{ marginTop: '0.5rem', marginBottom: '1.5rem', color: 'gray' }}>
+        マスターデュエルのSTANDARDレギュレーションで禁止制限になっているカードは記載していません。(例: ティアラメンツ・メイルゥなど)
+      </h2>
       <CardTable title="禁止カード" data={forbidden} color="red" iconPath="/img/forbidden.png" />
       <CardTable title="制限カード" data={limited} color="orange" iconPath="/img/limited.png" />
       <CardTable title="準制限カード" data={semiLimited} color="blue" iconPath="/img/semi_limited.png" />
