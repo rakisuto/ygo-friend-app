7895c4615655037ce1ef722bd052e24d910611a9
rakisuto <tama5.bo6bo6@gmail.com>
2026-06-26 16:58:15 +0900

    revert: 管理画面2階層化（2回目）をすべて差し戻し

--- ygo-friend-app/next.config.js
+++ ygo-friend-app/next.config.js
@@ -1,14 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/tournament/admin',
-        destination: '/admin',
-        permanent: true,
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
